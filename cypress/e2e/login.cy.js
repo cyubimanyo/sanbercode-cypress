ba6251a28caa14c8655a416d302b410b24966ca4
aye-kitalulus <andhiny.rizki@kitalulus.com>
2023-09-07 21:00:10 +0700

    login with 4 test cases

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,46 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('Login Success', () => {
+    // cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').type('John Doe')
+    cy.get('#txt-password').type('ThisIsNotAPassword')
+    cy.get('#btn-login').click()
+    cy.get('#appointment').should('be.visible')
+  })
+
+  it('Login with Invalid Username', () => {
+    // cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').type('Andhiny Fatikha')
+    cy.get('#txt-password').type('ThisIsNotAPassword')
+    cy.get('#btn-login').click()
+    cy.get('p').should(($p) => {
+      expect($p).to.contain('Login failed! Please ensure the username and password are valid.')
+    })
+  })
+
+  it('Login with Invalid Password', () => {
+    // cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').type('John Doe')
+    cy.get('#txt-password').type('Test123!')
+    cy.get('#btn-login').click()
+    cy.get('p').should(($p) => {
+      expect($p).to.contain('Login failed! Please ensure the username and password are valid.')
+    })
+  })
+
+  it('Login with Invalid Username and Password', () => {
+    // cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').type('Andhiny Fatikha')
+    cy.get('#txt-password').type('Test123!')
+    cy.get('#btn-login').click()
+    cy.get('p').should(($p) => {
+      expect($p).to.contain('Login failed! Please ensure the username and password are valid.')
+    })
+  })
+})
